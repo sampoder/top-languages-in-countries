527410598c5814ff42c14d1797212c89b4b03677
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-04-24 14:01:13 +0800

    links

--- components/emoji.js
+++ components/emoji.js
@@ -5,9 +5,7 @@ const Emoji = ({ emoji }) => (
   <>
     {!isIOS && !isMacOs ? (
       <>
-        <span
-          
-        ></span>
+        <span></span>
         <style>
           {`
       .emoji {
