f38eec32b1d6103471f5584f36afa05c9978a2b5
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-04-24 11:16:37 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -102,7 +102,7 @@ const Named = props => {
               sx={{
                 marginLeft: '0px',
                 fontWeight: [500, 800, 800],
-                mt: [1, 0, 0],
+                mt: [data[props.additionalData].first == "python" ? 2 : 1, 0, 0],
               }}
             >
               How about other countries?
